131c76fe0a9bfb6689ccbce141c1bb4f307ca435
wans <17340310@qq.com>
2020-10-22 16:11:34 +0800

    first

---
+++ mall-pms/src/main/java/com/wans/mall/pms/entity/SpuInfoDesc.java
@@ -0,0 +1,34 @@
+package com.wans.mall.pms.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * spu信息介绍
+ * 
+ * @author wans
+ * @date 2020-10-22 16:08:26
+ */
+@Data
+@TableName("pms_spu_info_desc")
+public class SpuInfoDesc implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 商品id
+	 */
+	@TableId
+	@ApiModelProperty(name = "spuId",value = "商品id")
+    private Long spuId;
+	/**
+	 * 商品介绍
+	 */
+	@ApiModelProperty(name = "decript",value = "商品介绍")
+    private String decript;
+
+}
